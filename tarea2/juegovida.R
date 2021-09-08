aed2fb80140e69f9fcca34ebfb9e147d4b3de117
eduartilon <88121775+eduartilon@users.noreply.github.com>
2021-09-07 23:36:37 -0500

    Update juegovida.R

--- tarea2/juegovida.R
+++ tarea2/juegovida.R
@@ -1,11 +1,12 @@
 library(parallel)
-dim <- 10
+library(ggplot2)
+dim <- 15
 num <-  dim^2
 p <- c(0.2, 0.25, 0.5,0.6,0.7)
 datos = data.frame()
-
+datos1 = data.frame()
 for (inicio in p) {
-  for (rep in 1:2) {
+  for (rep in 1:30) {
 actual <- matrix(round(runif(num) <= p), nrow=dim, ncol=dim, byrow=TRUE)
 paso <- function(pos) {
     fila <- floor((pos - 1) / dim) + 1
@@ -19,7 +20,7 @@ cluster <- makeCluster(detectCores() - 2)
 clusterExport(cluster, "dim")
 clusterExport(cluster, "paso")
 
-for (iteracion in 1:30) {
+for (iteracion in 1:50) {
      clusterExport(cluster, "actual")
      siguiente <- parSapply(cluster, 1:num, paso)
      vivos = sum(siguiente)
@@ -33,11 +34,19 @@ for (iteracion in 1:30) {
      }else { vivos = 1
      print("hay vida")
  }
- stopCluster(cluster)
+
+respuesta = c(inicio, vivos)
+datos = rbind(datos, respuesta) 
+stopCluster(cluster)
 }
-nvivos = c(vivos)
-print(nvivos)
-prob= (sum(nvivos))/2
-respuesta = c(inicio, prob)
-datos = rbind(datos, respuesta)
+
 }
+names(datos)<-c("probabilidad", "vida")
+datos1 = aggregate(vida~probabilidad, datos, mean)
+ggplot(datos1, aes(x = probabilidad, y = (vida*100) )) +
+geom_line()+geom_point()+
+  labs(x="Probabilidad al inicio", 
+       y="% de probabilidad de vida", 
+       title = "Probabilidad de vida a partir de un estado inicial dado")+
+  scale_x_continuous(limits = c(0,1))+
+  coord_cartesian(ylim = c(0,100))
