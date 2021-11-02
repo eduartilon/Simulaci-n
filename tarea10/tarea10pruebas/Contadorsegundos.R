2670f9a603817f8080dcd03a2e2982b8373befd8
eduartilon <88121775+eduartilon@users.noreply.github.com>
2021-11-02 09:13:36 -0600

    Update Contadorsegundos.R

--- tarea10/tarea10pruebas/Contadorsegundos.R
+++ tarea10/tarea10pruebas/Contadorsegundos.R
@@ -1,16 +1,16 @@
 
 library(tcltk)
 timer = 10
-pb <- tkProgressBar("Timer")
 start = Sys.time()
+
 while(TRUE) {
   elapsed = as.numeric(difftime(Sys.time(), start, units = 'secs'))
   remaining = timer - elapsed
   Sys.sleep(0.1)
-  setTkProgressBar(pb, remaining/timer, label = sprintf("Time remaining: %i seconds", round(remaining)))
+
   print(elapsed)
   if (remaining <= 0) break
  
 }
-Sys.sleep(2)
-close(pb)
+
+
